df5054f20483075740569600c7ae28ffbddb16cc
MadipaidgeManishKumar <154314402+6thSemsterMajorproject@users.noreply.github.com>
2023-12-20 12:11:14 +0530

    Update models.py

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -1,5 +1,6 @@
 from flaskapp import db
-class User(db.Model):
+from flask_login import UserMixin
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
